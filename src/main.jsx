7b45f6b40a3eadd37f4864bbc2125dfbef49fb4b
Vivek Rajput <work.tvivekrajput@gmail.com>
2026-06-11 10:25:38 +0530

    ms clarity using npm

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
-
+// ms clarity
+import Clarity from "@microsoft/clarity";
+Clarity.init("x579v2bz7e");
 
 
 createRoot(document.getElementById('root')).render(
